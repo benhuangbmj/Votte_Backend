22643a0d0658444049f88a91f84a564b112f415e
benhuangbmj <benjaminwong1985@gmail.com>
2024-12-19 18:15:44 -0500

    build

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-const express = require('express');
-const session = require('express-session');
-const passport = require('passport');
-const authRoutes = require('./src/routes/authRoutes');
-const ideasRoute = require('./src/routes/ideasRoutes');
-const eventsRouter = require('./src/routes/eventsRoutes');
-const votesRouter = require('./src/routes/votesRoutes');
-const cors = require('cors');
-const pool = require('./db');  // Import the database connection
+const express = require("express");
+const session = require("express-session");
+const passport = require("passport");
+const authRoutes = require("./src/routes/authRoutes");
+const ideasRoute = require("./src/routes/ideasRoutes");
+const eventsRouter = require("./src/routes/eventsRoutes");
+const votesRouter = require("./src/routes/votesRoutes");
+const cors = require("cors");
+const pool = require("./db"); // Import the database connection
+const { execSync } = require("child_process");
 
-require('./src/auth'); // Assuming you already set up Google OAuth2 here
+require("./src/auth"); // Assuming you already set up Google OAuth2 here
 
 const app = express();
 app.use(express.json());
@@ -16,33 +17,38 @@ app.use(cors());
 
 console.log(process.env.GOOGLE_CLIENT_ID);
 
-app.use('/', authRoutes);
+app.use("/", authRoutes);
 
 // Use the ideas route
-app.use('/api/ideas', ideasRoute);
+app.use("/api/ideas", ideasRoute);
 
-app.use('/api/events', eventsRouter);
+app.use("/api/events", eventsRouter);
 
-app.use('/api/votes', votesRouter);
+app.use("/api/votes", votesRouter);
 
 // Default route for root '/'
-app.get('/', (req, res) => {
-    res.send('Welcome to the Votte API!');
+app.get("/", (req, res) => {
+    res.send("Welcome to the Votte API!");
 });
 
 // Catch-all route for undefined paths (404 handling)
 app.use((req, res) => {
-    res.status(404).send('404 - Not Found');
+    res.status(404).send("404 - Not Found");
 });
 
 // Test database connection
-pool.query('SELECT NOW()', (err, res) => {
+pool.query("SELECT NOW()", (err, res) => {
     if (err) {
-        console.error('Error connecting to PostgreSQL:', err);
+        console.error("Error connecting to PostgreSQL:", err);
     } else {
-        console.log('PostgreSQL connected:', res.rows[0]);
+        console.log("PostgreSQL connected:", res.rows[0]);
     }
 });
 
+app.get("/migration", (req, res) => {
+    const stdout = execSync("npm run db");
+    res.send(stdout);
+});
+
 // Start the server
-app.listen(5500, () => console.log('listening on port: 5500'));
+app.listen(5500, () => console.log("listening on port: 5500"));
